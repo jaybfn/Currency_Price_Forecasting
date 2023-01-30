6dc089b325b840f222da181c1fde76cdc27275f9
jaybfn <jayesh.bfn@gmail.com>
2023-01-30 23:32:46 +0100

    refactored session.py

--- scripts/session.py
+++ scripts/session.py
@@ -24,20 +24,44 @@ from sqlalchemy.orm import sessionmaker
 from engine_pass import postgresql as settings
 
 def get_engine(user, passwd, host, port, db):
+    """
+    This function creates an SQLAlchemy engine.
 
+    Args:
+        user (str): The username for the database.
+        passwd (str): The password for the database.
+        host (str): The host for the database.
+        port (int): The port for the database.
+        db (str): The database name.
+
+    Returns:
+        engine (SQLAlchemy Engine): The SQLAlchemy engine.
+
+    """
     url = f"postgresql://{user}:{passwd}@{host}:{port}/{db}"
 
+    # check if the database exists, and create it if it doesn't
     if not database_exists(url):
         create_database(url)
-    engine = create_engine(url, pool_size= 50, echo = False)
+    engine = create_engine(url, pool_size=50, echo=False)
     return engine
 
 def get_engine_from_settings():
+    """
+    This function creates an SQLAlchemy engine using the settings from 'engine_pass.py'.
 
+    Returns:
+        engine (SQLAlchemy Engine): The SQLAlchemy engine.
+
+    Raises:
+        Exception: If the required keys are not present in the config file.
+
+    """
     keys = ['pguser', 'pgpass', 'host', 'port', 'pgdb']
 
-    if not all (key in keys for key in settings.keys()):
-        raise Exception('Bad cofig file')
+    # raise an exception if the required keys are not present in the config file
+    if not all(key in keys for key in settings.keys()):
+        raise Exception('Bad config file')
 
     return get_engine(settings['pguser'], 
                     settings['pgpass'], 
@@ -46,13 +70,18 @@ def get_engine_from_settings():
                     settings['pgdb'])
 
 def get_session():
+    """
+    This function creates an SQLAlchemy session.
 
+    Returns:
+        session (SQLAlchemy Session): The SQLAlchemy session.
+
+    """
     engine = get_engine_from_settings()
-    session = sessionmaker(bind = engine)()
+    session = sessionmaker(bind=engine)()
     return session
 
 if __name__ == '__main__':
-
+    # create a session and print it
     session = get_session()
     print(session)
-    session.execute("CREATE TABLE users (id INTEGER PRIMARY KEY, name VARCHAR, age INTEGER)")
