adfc51050b06754b9cc00a684f9ef9d458008246
jaybfn <jayesh.bfn@gmail.com>
2023-01-31 15:31:23 +0100

    adding new files

---
+++ scripts/engine_pass.py
@@ -0,0 +1,13 @@
+''' 
+        This script is a dictionary that contains configuration parameters for a PostgreSQL database connection. 
+        It includes information such as the username (pguser) with the value '******', the password (pgpass) with 
+        the value '******', the host (host) with the value 'localhost', the port (port) with the value 5432, and 
+        the database name (pgdb) with the value 'currency'. These parameters can be used to establish a connection 
+        to the PostgreSQL database with the specified information.
+'''
+postgresql = {'pguser': '******',
+                'pgpass': '*******',
+                'host': 'localhost',
+                'port': 5432,
+                'pgdb': 'currency'
+                }
